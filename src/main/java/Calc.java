92fafe5ccfa5bfdcc2f1529d81070d12005f0664
Yuta Odate <tutat@outlook.com>
2019-08-04 17:16:39 +0900

    init

---
+++ src/main/java/Calc.java
@@ -0,0 +1,6 @@
+public class Calc {
+
+    public static int calc(int x, int y){
+        return x + y;
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args){
+        int x = 3;
+        int y = 1;
+        System.out.println(Calc.calc(x, y));
+    }
+}
